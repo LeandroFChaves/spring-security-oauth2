83eacff619740843e5811a592506187109136eaa
LeandroFChaves <leandro.chaves@techne.com.br>
2019-03-21 17:55:02 -0300

    Defini página home como inicial

--- client1/src/app/app-routing.module.ts
+++ client1/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
