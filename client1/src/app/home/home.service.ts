fbb70582388cdcb4be760c582b87b917eab3a22d
LeandroFChaves <leandro.chaves@techne.com.br>
2019-03-21 18:10:15 -0300

    Defini proxy para utilização do client

--- client1/src/app/home/home.service.ts
+++ client1/src/app/home/home.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  private readonly API = 'api/common';
 
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public list() {
+    return this.http.get(this.API)
+    .pipe(
+      tap(console.log)
+    );
+  }
 }
--- client1/src/app/home/home/home.component.ts
+++ client1/src/app/home/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { empty } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { HomeService } from '../home.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private homeService: HomeService
+  ) { }
 
   ngOnInit() {
+  }
+
+  public testeProxyApi() {
+    this.homeService.list()
+     .pipe(
+       catchError(error => empty())
+     )
+    .subscribe(
+       dados => {
+         console.log(dados);
+       }
+       ,error => console.error(error),
+        () => console.log('Obserservable completo!')
+    );
   }
 
 }
