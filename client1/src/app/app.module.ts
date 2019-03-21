e1c7761aa6dda26ae4e7bec36afe046f1e14ea81
LeandroFChaves <leandro.chaves@techne.com.br>
2019-03-21 18:02:47 -0300

    Cria interceptor para acrescentar token bearer nas requisições http

--- client1/src/app/app.module.ts
+++ client1/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
+import { JwtHttpInterceptor } from './autenticacao/jwt-http.interceptor';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,7 @@ import { HomeModule } from './home/home.module';
   ],
 
   providers: [
-  
+    { provide: HTTP_INTERCEPTORS, useClass: JwtHttpInterceptor, multi: true },
   ],
 
   bootstrap: [
