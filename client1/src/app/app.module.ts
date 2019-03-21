206d2fd79f6162acd0221ae55a21a40bf2a97fda
LeandroFChaves <leandro.chaves@techne.com.br>
2019-03-21 17:50:12 -0300

    Criado página home (modulo, component, css e service) para utilização no exemplo

--- client1/src/app/app.module.ts
+++ client1/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HomeModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  providers: [
+  
+  ],
+
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
