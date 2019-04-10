26e4626c3ad6e019cdf06310628e504c0bf30f53
LeandroFChaves <leandro.chaves@techne.com.br>
2019-04-10 10:46:55 -0300

    Desenvolvimento do proxy responsável por rotear as chamadas para os recursos

---
+++ oauth-proxy/src/main/java/br/com/proxy/ProxyApplication.java
@@ -0,0 +1,15 @@
+package br.com.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+}
