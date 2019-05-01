b8dbf442757d87769b382ec504a7872f40a40a98
LeandroFChaves <leandro.fchaves@gmail.com>
2019-05-01 19:44:10 -0300

    Configurações para o Service Discovery - Eureka
    Realização de ajustes nos demais projetos para se registrarem no server eureka

--- oauth-proxy/src/main/java/br/com/proxy/ProxyApplication.java
+++ oauth-proxy/src/main/java/br/com/proxy/ProxyApplication.java
@@ -2,10 +2,12 @@ package br.com.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
 public class ProxyApplication {
 
 	public static void main(String[] args) {
